66bbe0fbdc24bbf3269790a6e6cee36b609e1bae
Tanmay Tripathi <wills4986@gmail.com>
2023-10-07 10:31:55 +0530

    Initial Commit

---
+++ Netflix Clone iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone iOS
+//
+//  Created by Tanmay Tripathi on 07/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Netflix Clone iOSTests/Netflix_Clone_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Netflix_Clone_iOSTests.swift
+//  Netflix Clone iOSTests
+//
+//  Created by Tanmay Tripathi on 07/10/23.
+//
+
+import XCTest
+@testable import Netflix_Clone_iOS
+
+final class Netflix_Clone_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
